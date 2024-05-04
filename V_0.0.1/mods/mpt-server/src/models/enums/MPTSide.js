9bf370d6a61431cb7c92c1fc29e039b3468cf8c9
Jaden-Allen <jadenallen1129@gmail.com>
2024-05-03 22:54:49 -0700

    V_0.0.1

--- V_0.0.1/mods/mpt-server/src/models/enums/MPTSide.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.MPTSide = void 0;
-var MPTSide;
-(function (MPTSide) {
-    MPTSide[MPTSide["PMC"] = 0] = "PMC";
-    MPTSide[MPTSide["Savage"] = 1] = "Savage";
-})(MPTSide || (exports.MPTSide = MPTSide = {}));
-//# sourceMappingURL=MPTSide.js.map
